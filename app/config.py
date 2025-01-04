432eaec70ffb8353e789421d9c28669488f99ddc
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-01-04 15:07:48 +0700

    memindahkan secret key dan menambahkan tipe session

--- app/config.py
+++ app/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '123'
+    SESSION_TYPE = 'filesystem'
