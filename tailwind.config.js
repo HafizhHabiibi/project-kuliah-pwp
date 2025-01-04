4b1cd473be66192b52b1a504ebd998964e6e799d
HafizhHabiibi <hafizhhabiibi123@gmail.com>
2025-01-05 00:30:02 +0700

    config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/templates/**/*.html",
+    "./static/src/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
